db89fb9842472123431ebc04ede40f0aa81ed54a
mikaelajintalan <mikaela.jintalan@gmail.com>
2021-04-25 21:27:40 +0800

    Move Footer to src

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+const Footer = (props) => {
+    return (
+        <footer>
+            <p>
+            This web site is owned and operated by Eternal Word Television Network, Inc. (EWTN), a public charity based in the United States.
+            <br />
+            <a href="https://www.ewtn.com/catholicism/saints" target="_blank" rel="noreferrer">Saints icon</a> by EWTN Global Catholic Network.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
